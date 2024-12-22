a4179d36dee1fa841d6390bf0812dfbb76a807df
berkayguzel06 <berkayguzel43@gmail.com>
2024-12-22 11:44:15 +0300

    TestPush

--- verification/src/main/java/com/example/verification/VerificationApplication.java
+++ verification/src/main/java/com/example/verification/VerificationApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class VerificationApplication {
 
 	public static void main(String[] args) {
+		// Test Application
 		SpringApplication.run(VerificationApplication.class, args);
 	}
 
